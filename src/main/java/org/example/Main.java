50c44982ce5ecd1710cb7e2058d802aa00e83b5f
Sokolov MN <Msokolov@maaail.ru>
2024-10-18 15:23:11 +0300

    Взял fil2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello file two world!");
-        System.out.println("Hello flyway world");
+        System.out.println("Hello flyway two world");
     }
 }
