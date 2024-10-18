9d10e800e7186dc71325d182134953f1aad35381
Sokolov MN <Msokolov@maaail.ru>
2024-10-18 16:23:12 +0300

    GIT COMMMIT MESSAGE

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello file 2 3 world!");
+        System.out.println("Hello file 2 3 wirld!");
         System.out.println("Hello flyway two world");
         System.out.println("file3");
+        System.out.println("flyway3");
+
     }
 }
