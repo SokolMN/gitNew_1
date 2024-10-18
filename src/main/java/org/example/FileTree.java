98b5fda58724a373f5dc23b05bd92e96825817fa
Sokolov MN <Msokolov@maaail.ru>
2024-10-18 16:17:53 +0300

    File3

---
+++ src/main/java/org/example/FileTree.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class FileTree {
+    String name;
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello file two world!");
+        System.out.println("Hello file 2 3 world!");
         System.out.println("Hello flyway two world");
+        System.out.println("file3");
     }
 }
