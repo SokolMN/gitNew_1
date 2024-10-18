4d92e7f3665f7973d794db8e54b682008ae6a16d
Sokolov MN <Msokolov@maaail.ru>
2024-10-18 15:20:39 +0300

    filetwo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello file world!");
+        System.out.println("Hello file two world!");
         System.out.println("Hello flyway world");
     }
 }
